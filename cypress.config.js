55e1d5f05771166238f4c2f7abd33f62bf36a1a1
yavor <yavor.ivanov162@gmail.com>
2025-11-24 16:22:30 +0200

    feat(cypress): Add cypress.config.js configuration file

---
+++ cypress.config.js
@@ -0,0 +1,12 @@
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+  e2e: {
+    specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
+    
+    baseUrl: process.env.CYPRESS_BASE_URL, 
+    
+    setupNodeEvents(on, config) {
+    },
+  },
+})
