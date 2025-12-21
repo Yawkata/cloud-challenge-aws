3ef333e0db0a8426f4faf59be5dee06df36fa402
yavor <yavor.ivanov162@gmail.com>
2025-12-21 16:16:18 +0200

    feat: Add Slack alert Lambda with SNS subscription

--- backend/lambdas/slack_notifier/slack_notifier.py
+++ backend/lambdas/slack_notifier/slack_notifier.py
@@ -1,2 +1,32 @@
+import json
+import boto3
+import urllib.request
+
+ssm = boto3.client('ssm')
+
 def lambda_handler(event, context):
-    print("Slack notifier lambda invoked")
+    parameter = ssm.get_parameter(
+        Name='/cloud-resume/slack/webhook',
+        WithDecryption=True
+    )
+    slack_webhook_url = parameter["Parameter"]["Value"]
+
+    message = {
+        "text": "ALERT!!! LMBDA IS NOT WORKING",
+        "icon_emoji": ":rotating_light:"
+    }
+
+    payload = {"\"text\": \"ALERT!!! LMBDA IS NOT WORKING\", \"icon_emoji\": \":rotating_light:\""}
+
+    req = urllib.request.Request(
+        slack_webhook_url,
+        data=json.dumps(message).encode("utf-8"),
+        headers={"Content-Type": "application/json"}
+    )
+
+    urllib.request.urlopen(req)
+
+    return {
+        "statusCode": 200,
+        "body": "Message was sent to Slack"
+    }
