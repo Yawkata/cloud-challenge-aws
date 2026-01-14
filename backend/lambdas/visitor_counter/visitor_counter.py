b961636857545802ef29530668ad69ddb6a20745
yavor <yavor.ivanov162@gmail.com>
2026-01-15 00:12:18 +0200

    feat: implement unique visitor tracking using IP de-duplication

--- backend/lambdas/visitor_counter/visitor_counter.py
+++ backend/lambdas/visitor_counter/visitor_counter.py
@@ -1,24 +1,31 @@
 import json
 import boto3
-import time
 
 dynamodb = boto3.resource('dynamodb')
-table = dynamodb.Table('VisitorCounterIAC')
+counter_table = dynamodb.Table('VisitorCounterIAC')
+ip_table = dynamodb.Table('VisitorIPsIAC')
 
 def lambda_handler(event, context):
-    response = table.get_item(Key={'id': 'visitor_count'})
+    visitor_ip = event['requestContext']['identity']['sourceIp']
 
-    response = table.update_item(
-        Key={'id': 'visitor_count'},
-        ExpressionAttributeNames={'#c': 'count'},
-        ExpressionAttributeValues={':inc': 1},
-        UpdateExpression="SET #c = #c + :inc",
-        ReturnValues="UPDATED_NEW"
-    )
+    ip_table_response = ip_table.get_item(Key={'ip': visitor_ip})
 
-    new_count = int(response['Attributes']['count'])
+    if 'Item' not in ip_table_response:
+        ip_table.put_item(Item={'ip': visitor_ip})
 
+        counter_table_response = counter_table.update_item(
+            Key={'id': 'visitor_count'},
+            ExpressionAttributeNames={'#c': 'count'},
+            ExpressionAttributeValues={':inc': 1},
+            UpdateExpression="SET #c = #c + :inc",
+            ReturnValues="UPDATED_NEW"
+        )
 
+        new_count = int(counter_table_response['Attributes']['count'])
+    else:
+        counter_table_response = counter_table.get_item(Key={'id': 'visitor_count'})
+        new_count = int(counter_table_response['Item']['count'])
+        
     return {
         'statusCode': 200,
         'headers': {
