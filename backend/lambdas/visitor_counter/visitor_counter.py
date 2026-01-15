08942201a7d3ad3446dd9b00132cd4cd40a0444b
yavor <yavor.ivanov162@gmail.com>
2026-01-15 18:03:50 +0200

    feat: implement salted IP hashing using AWS SSM

--- backend/lambdas/visitor_counter/visitor_counter.py
+++ backend/lambdas/visitor_counter/visitor_counter.py
@@ -1,17 +1,29 @@
 import json
 import boto3
+import hashlib
 
+ssm = boto3.client('ssm')
 dynamodb = boto3.resource('dynamodb')
 counter_table = dynamodb.Table('VisitorCounterIAC')
 ip_table = dynamodb.Table('VisitorIPsIAC')
 
+parameter = ssm.get_parameter(
+    Name='/cloud-resume/visitor-counter/ip-salt',
+    WithDecryption=True
+)
+IP_SALT = parameter["Parameter"]["Value"]
+
+def hash_ip(ip):
+    return hashlib.sha256(f"{IP_SALT}{ip}".encode()).hexdigest()
+
 def lambda_handler(event, context):
     visitor_ip = event['requestContext']['identity']['sourceIp']
+    hashed_visitor_ip = hash_ip(visitor_ip)
 
-    ip_table_response = ip_table.get_item(Key={'ip': visitor_ip})
+    ip_table_response = ip_table.get_item(Key={'ip': hashed_visitor_ip})
 
     if 'Item' not in ip_table_response:
-        ip_table.put_item(Item={'ip': visitor_ip})
+        ip_table.put_item(Item={'ip': hashed_visitor_ip})
 
         counter_table_response = counter_table.update_item(
             Key={'id': 'visitor_count'},
