f9310c81bde64f8ccfb1f7caf0f9bebd4cb58cde
yavor <yavor.ivanov162@gmail.com>
2026-01-23 21:35:34 +0200

    feat: Add logger info; remove Get method

--- backend/lambdas/visitor_counter/visitor_counter.py
+++ backend/lambdas/visitor_counter/visitor_counter.py
@@ -1,11 +1,16 @@
 import json
 import boto3
 import hashlib
+import logging
 
 ssm = boto3.client('ssm')
+
 dynamodb = boto3.resource('dynamodb')
 counter_table = dynamodb.Table('VisitorCounterIAC')
 ip_table = dynamodb.Table('VisitorIPsIAC')
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
 parameter = ssm.get_parameter(
     Name='/cloud-resume/visitor-counter/ip-salt',
@@ -16,13 +21,16 @@ IP_SALT = parameter["Parameter"]["Value"]
 def hash_ip(ip):
     return hashlib.sha256(f"{IP_SALT}{ip}".encode()).hexdigest()
 
+def is_new_visitor(hashed_ip):
+    response = ip_table.get_item(Key={'ip': hashed_ip})
+    return 'Item' not in response
+
 def lambda_handler(event, context):
     visitor_ip = event['requestContext']['identity']['sourceIp']
     hashed_visitor_ip = hash_ip(visitor_ip)
 
-    ip_table_response = ip_table.get_item(Key={'ip': hashed_visitor_ip})
-
-    if 'Item' not in ip_table_response:
+    if is_new_visitor(hashed_visitor_ip):
+        decision = 'new_visitor'
         ip_table.put_item(Item={'ip': hashed_visitor_ip})
 
         counter_table_response = counter_table.update_item(
@@ -35,9 +43,16 @@ def lambda_handler(event, context):
 
         new_count = int(counter_table_response['Attributes']['count'])
     else:
+        decision = 'existing_visitor'
         counter_table_response = counter_table.get_item(Key={'id': 'visitor_count'})
         new_count = int(counter_table_response['Item']['count'])
-        
+
+    logger.info({
+            'visitor_ip_hash': hashed_visitor_ip,
+            'decision': decision,
+            'visitor_count': new_count
+        })
+
     return {
         'statusCode': 200,
         'headers': {
--- web/api.js
+++ web/api.js
@@ -1,11 +1,5 @@
 const API_BASE_URL = "https://vi9ausvov7.execute-api.eu-north-1.amazonaws.com/test";
 
-export async function getVisitorCount() {
-    const response = await fetch(`${API_BASE_URL}/visitor`);
-    const data = await response.json();
-    return data.visitor_count;
-}
-
 export async function incrementVisitorCount() {
     const response = await fetch(`${API_BASE_URL}/visitor`, {
         method: "POST"
